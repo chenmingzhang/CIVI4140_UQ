6a58b7c7b49b53663937a46e7288527a3a99e8c3
Chenming Zhang <uqczhan2@mango.eait.uq.edu.au>
2019-08-29 17:03:35 +1000

    add the past files

--- Q1_flopy/q1.py
+++ Q1_flopy/q1.py
@@ -7,37 +7,48 @@ Lx = 100. # from plot, y is plotted from left to right
 Ly = 10.  # from plot, y is plotted upward
 ztop = 0.
 zbot = -20.
-nlay = 10
-#nlay = 1
+#nlay = 10
+nlay = 1
 nrow = 1
 ncol = 10
 delr = Lx / ncol
 delc = Ly / nrow
 delv = (ztop - zbot) / nlay
 botm = np.linspace(ztop, zbot, nlay + 1)
-hk = 0.06   # how to change the time unit in flopy?
-vka = 0.06
+
+#dis.itmuni_dict
+#dis.itmuni
+hk = 30   # how to change the time unit in flopy?  ITMUNI in dis #dis.itmuni_dict
+vka = 30
 sy = 0.25
 ss = 1.e-4
 laytyp = 1   # unconfined
 
+
+
 # Variables for the BAS package
 ibound = np.ones((nlay, nrow, ncol), dtype=np.int32) + 2
-ibound[0][0][0]=1   # why do we need three layers here??
-strt = 10. * np.ones((nlay, nrow, ncol), dtype=np.float32)
+lay_id_chd=0
+row_id_chd=0
+col_id_chd=0
+ibound_chd=1
+ibound[lay_id_chd][row_id_chd][col_id_chd]=ibound_chd   # why do we need three layers here?? because the model is 3-d
+#ibound[lay_id_chd][row_id_chd][1]=ibound_chd   # why do we need three layers here?? because the model is 3-d
 
+strt = 10. * np.ones((nlay, nrow, ncol), dtype=np.float32)
 
 # time step parameters
 nper = 3
-perlen = [1, 100, 100]
-nstp = [1, 100, 100]
+perlen =[100, 100, 100]  # in days i guess
+nstp = [100, 100, 100]
 steady = [True, False, False]
 
 modelname = 'tutorial2'
 mf = flopy.modflow.Modflow(modelname, exe_name='mf2005')
 dis = flopy.modflow.ModflowDis(mf, nlay, nrow, ncol, delr=delr, delc=delc,
                                        top=ztop, botm=botm[1:],
-                                      nper=nper, perlen=perlen, nstp=nstp, steady=steady)
+                                      nper=nper, perlen=perlen, nstp=nstp, steady=steady,
+                                      itmuni=4)
 bas = flopy.modflow.ModflowBas(mf, ibound=ibound, strt=strt)
 lpf = flopy.modflow.ModflowLpf(mf, hk=hk, vka=vka, sy=sy, ss=ss, laytyp=laytyp, ipakcb=53)
 pcg = flopy.modflow.ModflowPcg(mf)
@@ -55,20 +66,12 @@ quadmesh = modelmap.plot_ibound()  # a useful command to check all the ibounds
 fig.show()
 
 
-fig = plt.figure(figsize=(8, 3))
-ax = fig.add_subplot(1, 1, 1)
-#modelxsect = flopy.plot.ModelCrossSection(model=mf, line={'Column': 5})  # this will only work when nrow is more than 1
-modelxsect = flopy.plot.ModelCrossSection(model=mf, line={'Row': 0})
-patches = modelxsect.plot_ibound()
-linecollection = modelxsect.plot_grid()
-t = ax.set_title('Row 0 Cross-Section with IBOUND Boundary Conditions')
-fig.show()
 
 #dis.sr.xcentergrid
 #dis.sr.ycentergrid
 #dis.sr.xgrid
 #dis.sr.ygrid
-#np.ma.masked_equal(dis.sr.ygrid,1000) # very useful command
+#np.ma.masked_equal(dis.sr.ygrid,1000) # very useful command to find specific file locations
 #modelmap.sr.vertices
 #flopy.plot.plotutil.cell_value_points
 #
@@ -81,13 +84,52 @@ fig.show()
 #modelxsect.xcentergrid
 #modelxsect.zcentergrid
 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+# ibound nlay,nrow,ncol
+#slicing a row for crosection plot
+
+ib=ibound[:,0,:]
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+modelxsect = flopy.plot.ModelCrossSection(model=mf, line={'Row': 0})
+surf = ax.plot_surface(modelxsect.xcentergrid, modelxsect.zcentergrid, ib, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+plt.show(block=False)
 
 
+'''
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
+'''
 
-
-
-
+# we are supposed to use CHD boundary here not GHB boundary
+'''
 # Make list for stress period 1
 stageleft = 10.
 stageright = 10.
@@ -119,20 +161,89 @@ stress_period_data = {0: bound_sp1, 1: bound_sp2}
 
 # Create the flopy ghb object
 ghb = flopy.modflow.ModflowGhb(mf, stress_period_data=stress_period_data)
+'''
+
+#chd={0:[
+#       [0,0,0,1,2],
+#       [0,0,2,1,2],
+#       ]
+#    }
+
+ibound_chd_mask=np.ma.masked_equal(ibound,ibound_chd)
+
+chd_node_index=np.where(ibound_chd_mask.mask)
+stress_period_data = {}
+
+bound_sp0=[]
+for i in np.arange(np.sum(ibound_chd_mask.mask)):
+   bound_sp0.append([chd_node_index[0][i],chd_node_index[1][i],chd_node_index[2][i],0,0]  )
+bound_sp1=[]
+for i in np.arange(np.sum(ibound_chd_mask.mask)):
+   bound_sp1.append([chd_node_index[0][i],chd_node_index[1][i],chd_node_index[2][i],-10,-10]  )
+
+bound_sp2=[]
+for i in np.arange(np.sum(ibound_chd_mask.mask)):
+   bound_sp2.append([chd_node_index[0][i],chd_node_index[1][i],chd_node_index[2][i],0,0]  )
 
 
 
+stress_period_data={0:bound_sp0,1:bound_sp1,2:bound_sp2}
 
-tress_period_data = {}
+chd=flopy.modflow.mfchd.ModflowChd(model=mf,stress_period_data=stress_period_data)
+
+
+#flopy.modflow.mfchd.ModflowChd(model=mf,stress_period_data=chd)
+
+stress_period_data = {}
 for kper in range(nper):
     for kstp in range(nstp[kper]):
-        stress_period_data[(kper, kstp)] = ['save head',
-                                            'save drawdown',
-                                            'save budget',
-                                            'print head',
-                                            'print budget']
-oc = flopy.modflow.ModflowOc(mf, stress_period_data=stress_period_data,
-                             compact=True)
+        if np.mod(kstp,50)==0:
+            stress_period_data[(kper, kstp)] = ['save head',
+                                                'save drawdown',
+                                                'save budget',
+                                                'print head',
+                                                'print budget']
+oc = flopy.modflow.ModflowOc(mf, stress_period_data=stress_period_data,compact=True)
+
+
+mf.write_input()
+
+# Run the model
+success, mfoutput = mf.run_model(silent=True, pause=False, report=True)
+if not success:
+        raise Exception('MODFLOW did not terminate normally.')
+
+
+# Imports
+import matplotlib.pyplot as plt
+import flopy.utils.binaryfile as bf
+
+
+# Create the headfile and budget file objects
+headobj = bf.HeadFile(modelname+'.hds')
+times = headobj.get_times()
+cbb = bf.CellBudgetFile(modelname+'.cbc')
+
+
+mytimes = [1.0, 101.0, 201.0]
+
+
+fig = plt.figure(figsize=(8, 3))
+ax = fig.add_subplot(1, 1, 1)
+#modelxsect = flopy.plot.ModelCrossSection(model=mf, line={'Column': 5})  # this will only work when nrow is more than 1
+modelxsect = flopy.plot.ModelCrossSection(model=mf, line={'Row': 0})
+patches = modelxsect.plot_ibound()
+linecollection = modelxsect.plot_grid()
+t = ax.set_title('Row 0 Cross-Section with IBOUND Boundary Conditions')
+
+#plot(dis.sr.xcentergrid.shape,head[0,0,:])
+head = headobj.get_data(totim=mytimes[2])
+ax.plot(dis.sr.xcentergrid[0,:],head[0,0,:])
+head = headobj.get_data(totim=mytimes[1])
+ax.plot(dis.sr.xcentergrid[0,:],head[0,0,:])
+
+fig.show()
+
 
 
 
